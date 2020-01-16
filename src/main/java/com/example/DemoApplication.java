78bbead22370b0e39824d7271adbd4f0741a5d30
jonathan <jonathan.soleillet@gmail.com>
2020-01-16 10:15:00 +0100

    Modification du code pour respect des règles checkstyle

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -3,10 +3,25 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+* Classe principal de l'application sample spring boot.
+**/
 @SpringBootApplication
-public class DemoApplication {
+final class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+        /**
+        * Constructeur privé.
+        * lève une exception si instanciation de cette classe.
+        **/
+        private DemoApplication() {
+        }
+
+        /**
+        * Méthode principale de DemoApplication.
+        * Lance l'application Spring-Boot.
+        * @param args : arguement de l'application
+        **/
+        public static void main(final String[] args) {
+          SpringApplication.run(DemoApplication.class, args);
+        }
 }
