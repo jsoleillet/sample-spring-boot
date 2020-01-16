d63d2caada89df899847c66a796a6c6051874d16
jonathan <jonathan.soleillet@gmail.com>
2020-01-16 11:54:27 +0100

    Ajout de test, failed et skipped for test rapport on jenkins

--- src/test/java/com/example/DemoApplicationTests.java
+++ src/test/java/com/example/DemoApplicationTests.java
@@ -1,6 +1,9 @@
 package com.example;
 
 import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+import org.junit.Ignore;
+
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -12,5 +15,20 @@ public class DemoApplicationTests {
 	@Test
 	public void contextLoads() {
 	}
+
+        @Ignore("This test is ignored")
+        @Test
+        public void testIgnore() {
+        }
+
+        @Test
+        public void shouldPass() {
+          assertTrue(true);
+        }
+
+        @Test
+        public void alwaysFail() {
+          assertTrue(false);
+        }
 
 }
